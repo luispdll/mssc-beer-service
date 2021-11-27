0aa775ae55818d6149d0ae2190ea702356782c64
Francisco Hernandez <frankph0198@gmail.com>
2021-11-27 12:47:29 -0600

    Initial Project 2

---
+++ src/test/java/io/thinkingcode/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thinkingcode.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
