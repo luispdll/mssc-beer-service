f32c659bc1768cbf0ecd5e6558951be986ee6ca6
Francisco Hernandez <frankph0198@gmail.com>
2022-07-09 14:58:06 -0500

    Fixing Mapper

--- src/main/java/io/thinkingcode/msscbeerservice/common/events/ValidateOrderRequest.java
+++ src/main/java/io/thinkingcode/msscbeerservice/common/events/ValidateOrderRequest.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import io.thinkingcode.msscbeerservice.common.events.BeerOrderDTO;
-import thinkingcode.sfg.brewery.model.BeerOrderDTO;
 
 @Data
 @NoArgsConstructor
--- src/main/java/io/thinkingcode/msscbeerservice/web/mappers/BeerMapper.java
+++ src/main/java/io/thinkingcode/msscbeerservice/web/mappers/BeerMapper.java
@@ -2,9 +2,11 @@ package io.thinkingcode.msscbeerservice.web.mappers;
 
 import io.thinkingcode.msscbeerservice.common.BeerDTO;
 import io.thinkingcode.msscbeerservice.domain.Beer;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = {DateMapper.class})
+@Mapper(uses = {DateMapper.class}, componentModel = "spring")
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
 
     BeerDTO beerToBeerDto(Beer beer);
