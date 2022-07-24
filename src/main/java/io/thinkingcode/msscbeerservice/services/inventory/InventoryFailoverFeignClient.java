c521c1d9bf97ce5d1c6d7071548f63dfd4abc2ea
Francisco Hernandez <frankph0198@gmail.com>
2022-07-24 16:03:28 -0500

    Eureka

--- src/main/java/io/thinkingcode/msscbeerservice/services/inventory/InventoryFailoverFeignClient.java
+++ src/main/java/io/thinkingcode/msscbeerservice/services/inventory/InventoryFailoverFeignClient.java
@@ -3,12 +3,14 @@ package io.thinkingcode.msscbeerservice.services.inventory;
 import io.thinkingcode.msscbeerservice.services.inventory.model.BeerInventoryDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @FeignClient(name = "inventory-failover")
+@Component
 public interface InventoryFailoverFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/inventory-failover")
